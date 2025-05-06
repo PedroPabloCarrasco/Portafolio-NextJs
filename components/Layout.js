c2025dedcb7373c25081d8c444cd7420fe19e34a
Pedro Pablo Carrasco <pedropablocarrascob@gmail.com>
2025-05-06 19:55:55 -0400

    NAvBar con componentes dentrados

--- components/Layout.js
+++ components/Layout.js
@@ -3,7 +3,10 @@ import Navbar from "./Navbar";
 const Layout = ({ children }) => (
     <>
         <Navbar />
+        <main className="container py-4">
         {children}
+
+        </main>
     </>
 );
 
--- components/Navbar.js
+++ components/Navbar.js
@@ -1,33 +1,31 @@
+import Link from "next/link";
+
+
+
 const Navbar = () => (
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
 
         <div className="container">
+
+            <Link href="/" className="navbar-brand">PortFolio</Link>
+
+
             
-                <a className="navbar-brand" href="#">PortFolio</a>
+                
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarNavDropdown">
-                    <ul className="navbar-nav">
+                    <ul className="navbar-nav ms-auto">
+                        
                         <li className="nav-item">
-                            <a className="nav-link active" aria-current="page" href="#">Home</a>
+                            <Link href="/blog" className="nav-link" >Blog</Link>
+                            
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="#">Features</a>
+                            <Link href="/github" className="nav-link">Github</Link>
                         </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="#">Pricing</a>
-                        </li>
-                        <li className="nav-item dropdown">
-                            <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                Dropdown link
-                            </a>
-                            <ul className="dropdown-menu">
-                                <li><a className="dropdown-item" href="#">Action</a></li>
-                                <li><a className="dropdown-item" href="#">Another action</a></li>
-                                <li><a className="dropdown-item" href="#">Something else here</a></li>
-                            </ul>
-                        </li>
+                        
                     </ul>
                 </div>
             
