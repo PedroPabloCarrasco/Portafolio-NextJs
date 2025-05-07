4086a1948cf678f2db9934fbfa0258e0eaf9c111
Pedro Pablo Carrasco <pedropablocarrascob@gmail.com>
2025-05-06 20:37:22 -0400

    Se agrega descripción y head

--- components/Layout.js
+++ components/Layout.js
@@ -1,7 +1,13 @@
 import Navbar from "./Navbar";
+import Head from "next/head";
 
 const Layout = ({ children }) => (
     <>
+
+    <Head> 
+        <title>Mi PortFolio</title>
+    </Head>
+    
         <Navbar />
         <main className="container py-4">
         {children}
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,37 @@ import Layout from "../../components/Layout";
 const Index = () => 
 (
 <Layout>
-<h1>Hello</h1>
+
+{ /**Header Card */}
+<header className="row">
+    <div className="col-md-12">
+        <div className="card card-body bg-secondary text-light">
+            <div className="row">
+                <div className="col-md-4">
+                    <img src="ryan-ray-profile2.jpeg" alt="" className="img-fluid"/>
+                </div>
+                <div className="col-md-8">
+                    <h1>Pedro Pablo Carrasco</h1>
+                    <h4>Fullstack developer junior</h4>
+                    <p>
+                    Do consequat laboris enim culpa labore et voluptate do id officia 
+                    magna adipisicing incididunt ut.
+                    magna adipisicing incididunt ut
+                    magna adipisicing incididunt ut.
+                    magna adipisicing incididunt ut
+                    </p>
+                    <a href="/contrato">Contratame</a>
+
+                </div>
+                
+            </div>
+        </div>
+
+    </div>
+</header>
+
+
+
 </Layout>
 
 
